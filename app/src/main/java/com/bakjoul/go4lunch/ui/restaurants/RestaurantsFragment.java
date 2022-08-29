57f60e6e05df0cfbb4be7e0288a2a14e80f40c61
bakjoul <bakjoul@gmail.com>
2022-08-29 13:16:06 +0200

    Removed fragment show/hide test in RestaurantsFragment

--- app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsFragment.java
+++ app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsFragment.java
@@ -13,45 +13,16 @@ import com.bakjoul.go4lunch.databinding.FragmentRestaurantsBinding;
 
 public class RestaurantsFragment extends Fragment {
 
-    private static final String KEY_CLICKED = "KEY_CLICKED";
-
     public static RestaurantsFragment newInstance() {
         return new RestaurantsFragment();
     }
 
     private FragmentRestaurantsBinding binding;
 
-    private int clicked = 0;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentRestaurantsBinding.inflate(inflater, container, false);
         return binding.getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        if (savedInstanceState != null) {
-            clicked = savedInstanceState.getInt(KEY_CLICKED);
-        }
-
-        binding.listButton.setText("" + clicked);
-        binding.listButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clicked++;
-                binding.listButton.setText("" + clicked);
-            }
-        });
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putInt(KEY_CLICKED, clicked);
     }
 }
