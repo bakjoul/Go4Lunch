d7571dd8fe36b1c25587ca2641a51fb89e6649ca
bakjoul <bakjoul@gmail.com>
2022-09-02 17:24:39 +0200

    Removed onActivityResult override, refactored LoginActivity

--- app/src/main/java/com/bakjoul/go4lunch/ui/login/LoginActivity.java
+++ app/src/main/java/com/bakjoul/go4lunch/ui/login/LoginActivity.java
@@ -9,7 +9,6 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bakjoul.go4lunch.R;
@@ -26,7 +25,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -57,14 +55,8 @@ public class LoginActivity extends AppCompatActivity {
         firebaseAuth = FirebaseAuth.getInstance();
 
         setBackground();
-
-        SignInButton signInButton = findViewById(R.id.login_button_google2);
-        signInButton.setSize(SignInButton.SIZE_WIDE);
-
         initFacebookLoginButton();
-        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::onGoogleSignInResult);
-
-        binding.loginButtonGoogle.setOnClickListener(view -> googleSignIn());
+        initGoogleLoginButton();
     }
 
     private void setBackground() {
@@ -73,28 +65,6 @@ public class LoginActivity extends AppCompatActivity {
             .load(R.drawable.bkg_colleagues_lunch)
             .transform(new BlurTransformation(25, 1))
             .into(background);
-    }
-
-    private void googleSignIn() {
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.google_server_client_id))
-            .requestEmail()
-            .build();
-        GoogleSignInClient signInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
-        activityResultLauncher.launch(signInClient.getSignInIntent());
-    }
-
-    private void onGoogleSignInResult(@NonNull ActivityResult result) {
-        if (result.getResultCode() == RESULT_OK) {
-            Intent data = result.getData();
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                handleGoogleAccessToken(account);
-            } catch (ApiException e) {
-                Log.w(TAG, "Google sign in failed", e);
-            }
-        }
     }
 
     private void initFacebookLoginButton() {
@@ -119,14 +89,8 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         binding.loginButtonFacebook.setOnClickListener(view ->
-            LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, Arrays.asList("email", "public_profile")));
+            LoginManager.getInstance().logInWithReadPermissions(LoginActivity.this, callbackManager, Arrays.asList("email", "public_profile")));
 
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     private void handleFacebookAccessToken(@NonNull AccessToken token) {
@@ -144,6 +108,33 @@ public class LoginActivity extends AppCompatActivity {
                     Log.w(TAG, "signInWithCredential:failure", task.getException());
                 }
             });
+    }
+
+    private void initGoogleLoginButton() {
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), this::onGoogleSignInResult);
+        binding.loginButtonGoogle.setOnClickListener(view -> googleSignIn());
+    }
+
+    private void googleSignIn() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.google_server_client_id))
+            .requestEmail()
+            .build();
+        GoogleSignInClient signInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+        activityResultLauncher.launch(signInClient.getSignInIntent());
+    }
+
+    private void onGoogleSignInResult(@NonNull ActivityResult result) {
+        if (result.getResultCode() == RESULT_OK) {
+            Intent data = result.getData();
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                handleGoogleAccessToken(account);
+            } catch (ApiException e) {
+                Log.w(TAG, "Google sign in failed", e);
+            }
+        }
     }
 
     private void handleGoogleAccessToken(@NonNull GoogleSignInAccount account) {
