a77f50f84abb36ad8e5d6c4d64aabfa266a15e5c
bakjoul <bakjoul@gmail.com>
2022-09-05 13:11:48 +0200

    Created RestaurantsAdapter, refactored RestaurantsItemViewState

---
+++ app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsAdapter.java
@@ -0,0 +1,62 @@
+package com.bakjoul.go4lunch.ui.restaurants;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bakjoul.go4lunch.databinding.FragmentRestaurantsItemBinding;
+
+public class RestaurantsAdapter extends ListAdapter<RestaurantsItemViewState, RestaurantsAdapter.ViewHolder> {
+
+    public RestaurantsAdapter() {
+        super(new RestaurantsAdapterDiffCallback());
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(FragmentRestaurantsItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private final FragmentRestaurantsItemBinding binding;
+
+        public ViewHolder(@NonNull FragmentRestaurantsItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+
+        public void bind(@NonNull RestaurantsItemViewState restaurantsItemViewState) {
+            binding.restaurantsItemName.setText(restaurantsItemViewState.getName());
+            binding.restaurantsItemLocation.setText(restaurantsItemViewState.getLocation());
+            binding.restaurantsItemOpeningTimes.setText(restaurantsItemViewState.getOpeningTimes());
+            binding.restaurantsItemDistance.setText(restaurantsItemViewState.getDistance());
+            binding.restaurantsItemAttendance.setText(restaurantsItemViewState.getAttendance());
+            binding.restaurantsItemRating.setRating(restaurantsItemViewState.getRating());
+            // TODO Photo
+        }
+    }
+
+    private static class RestaurantsAdapterDiffCallback extends DiffUtil.ItemCallback<RestaurantsItemViewState> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull RestaurantsItemViewState oldItem, @NonNull RestaurantsItemViewState newItem) {
+            return oldItem.getId().equals(newItem.getId());
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull RestaurantsItemViewState oldItem, @NonNull RestaurantsItemViewState newItem) {
+            return oldItem.equals(newItem);
+        }
+    }
+}
--- app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsItemViewState.java
+++ app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsItemViewState.java
@@ -1,5 +1,7 @@
 package com.bakjoul.go4lunch.ui.restaurants;
 
+import android.net.Uri;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
@@ -12,6 +14,8 @@ public class RestaurantsItemViewState {
     @NonNull
     private final String name;
     @NonNull
+    private final String location;
+    @NonNull
     private final String openingTimes;
     @NonNull
     private final String distance;
@@ -20,18 +24,25 @@ public class RestaurantsItemViewState {
 
     private final int rating;
 
+    @Nullable
+    private final Uri photo;
+
     public RestaurantsItemViewState(@NonNull String id,
                                     @NonNull String name,
+                                    @NonNull String location,
                                     @NonNull String openingTimes,
                                     @NonNull String distance,
                                     @NonNull String attendance,
-                                    int rating) {
+                                    int rating,
+                                    @Nullable Uri photo) {
         this.id = id;
         this.name = name;
+        this.location = location;
         this.openingTimes = openingTimes;
         this.distance = distance;
         this.attendance = attendance;
         this.rating = rating;
+        this.photo = photo;
     }
 
     @NonNull
@@ -42,6 +53,11 @@ public class RestaurantsItemViewState {
     @NonNull
     public String getName() {
         return name;
+    }
+
+    @NonNull
+    public String getLocation() {
+        return location;
     }
 
     @NonNull
@@ -63,17 +79,22 @@ public class RestaurantsItemViewState {
         return rating;
     }
 
+    @Nullable
+    public Uri getPhoto() {
+        return photo;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         RestaurantsItemViewState that = (RestaurantsItemViewState) o;
-        return rating == that.rating && id.equals(that.id) && name.equals(that.name) && openingTimes.equals(that.openingTimes) && distance.equals(that.distance) && attendance.equals(that.attendance);
+        return rating == that.rating && id.equals(that.id) && name.equals(that.name) && location.equals(that.location) && openingTimes.equals(that.openingTimes) && distance.equals(that.distance) && attendance.equals(that.attendance) && Objects.equals(photo, that.photo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, openingTimes, distance, attendance, rating);
+        return Objects.hash(id, name, location, openingTimes, distance, attendance, rating, photo);
     }
 
     @NonNull
@@ -82,10 +103,12 @@ public class RestaurantsItemViewState {
         return "RestaurantsItemViewState{" +
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
+            ", location='" + location + '\'' +
             ", openingTimes='" + openingTimes + '\'' +
             ", distance='" + distance + '\'' +
             ", attendance='" + attendance + '\'' +
             ", rating=" + rating +
+            ", photo=" + photo +
             '}';
     }
 }
