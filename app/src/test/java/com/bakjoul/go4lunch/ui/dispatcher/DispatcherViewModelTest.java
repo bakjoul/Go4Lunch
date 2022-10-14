e2096a4d6d51934e694d2719e477de37ebc561ac
bakjoul <bakjoul@gmail.com>
2022-10-14 19:46:59 +0200

    Added a test in DispatcherViewModelTest

--- app/src/test/java/com/bakjoul/go4lunch/ui/dispatcher/DispatcherViewModelTest.java
+++ app/src/test/java/com/bakjoul/go4lunch/ui/dispatcher/DispatcherViewModelTest.java
@@ -1,11 +1,15 @@
 package com.bakjoul.go4lunch.ui.dispatcher;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 public class DispatcherViewModelTest {
@@ -22,4 +26,27 @@ public class DispatcherViewModelTest {
       viewModel = new DispatcherViewModel(firebaseAuth);
    }
 
+   @Test
+   public void currentUser_null_should_expose_go_to_connect_screen_view_action() {
+      // Given
+      doReturn(null).when(firebaseAuth).getCurrentUser();
+
+      // When
+      DispatcherViewAction result = viewModel.getViewActionSingleLiveEvent().getValue();
+
+      // Then
+      assertEquals(DispatcherViewAction.GO_TO_CONNECT_SCREEN, result);
+   }
+
+/*   @Test
+   public void currentUser_non_null_should_expose_go_to_main_screen_view_action() {
+      // Given
+      doReturn().when(firebaseAuth).getCurrentUser();
+
+      // When
+      DispatcherViewAction result = viewModel.getViewActionSingleLiveEvent().getValue();
+
+      // Then
+      assertEquals(DispatcherViewAction.GO_TO_MAIN_SCREEN, result);
+   }*/
 }
