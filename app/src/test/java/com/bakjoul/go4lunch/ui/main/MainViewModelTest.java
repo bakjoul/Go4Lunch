95394252f98941bb4772e9afdd93ccd962f1f161
bakjoul <bakjoul@gmail.com>
2022-10-17 12:48:53 +0200

    Creating MainViewModelTest

---
+++ app/src/test/java/com/bakjoul/go4lunch/ui/main/MainViewModelTest.java
@@ -0,0 +1,32 @@
+package com.bakjoul.go4lunch.ui.main;
+
+import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import com.bakjoul.go4lunch.data.repository.LocationRepository;
+import com.bakjoul.go4lunch.data.repository.PermissionRepository;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.mockito.Mockito;
+
+public class MainViewModelTest {
+
+   @Rule
+   public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+   private final Context context = Mockito.mock(Context.class);
+   private final FirebaseAuth firebaseAuth = Mockito.mock(FirebaseAuth.class);
+   private final LocationRepository locationRepository = Mockito.mock(LocationRepository.class);
+   private final PermissionRepository permissionRepository = Mockito.mock(PermissionRepository.class);
+   
+   private MainViewModel viewModel;
+
+   @Before
+   public void setUp() {
+      viewModel = new MainViewModel(context, firebaseAuth, locationRepository, permissionRepository);
+   }
+
+}
