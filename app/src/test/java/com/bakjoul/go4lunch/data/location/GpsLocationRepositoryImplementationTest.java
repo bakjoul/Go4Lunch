df30d6a82f6d4cdccee6db26b86f3bdf5c953e53
bakjoul <bakjoul@gmail.com>
2023-02-07 11:48:36 +0100

    Refactored GpsLocationRepositoryImplementationTest

--- app/src/test/java/com/bakjoul/go4lunch/data/location/GpsLocationRepositoryImplementationTest.java
+++ app/src/test/java/com/bakjoul/go4lunch/data/location/GpsLocationRepositoryImplementationTest.java
@@ -2,6 +2,7 @@ package com.bakjoul.go4lunch.data.location;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 
 import android.location.Location;
@@ -19,7 +20,6 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.stubbing.Answer;
 
@@ -38,7 +38,7 @@ public class GpsLocationRepositoryImplementationTest {
     }
 
     @Test
-    public void startLocationUpdates_should_allow_location_permission() {
+    public void startLocationUpdates_should_expose_new_location() {
         // Given
         ArgumentCaptor<LocationCallback> locationCallbackArgumentCaptor = ArgumentCaptor.forClass(LocationCallback.class);
         LocationResult locationResult = Mockito.mock(LocationResult.class);
@@ -58,12 +58,15 @@ public class GpsLocationRepositoryImplementationTest {
         );
         locationCallbackArgumentCaptor.getValue().onLocationResult(locationResult);
 
+        Boolean isPermissionGranted = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getIsLocationPermissionAllowedLiveData());
+
         // Then
         assertEquals(location, liveData.getValue());
+        assertTrue(isPermissionGranted);
     }
 
     @Test
-    public void startLocationUpdates_should_allow_location_permission_bis() {
+    public void startLocationUpdates_should_expose_new_location_bis() {
         // Given
         LocationResult locationResult = Mockito.mock(LocationResult.class);
         Location location = Mockito.mock(Location.class);
@@ -77,20 +80,32 @@ public class GpsLocationRepositoryImplementationTest {
 
         // When
         Location result = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getCurrentLocationLiveData());
+        Boolean isPermissionGranted = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getIsLocationPermissionAllowedLiveData());
 
         // Then
         assertEquals(location, result);
+        assertTrue(isPermissionGranted);
     }
 
     @Test
-    public void stopLocationUpdates_should_disallow_location_permission() {
+    public void stopLocationUpdates_should_expose_location_null() {
         // Given
+        LocationResult locationResult = Mockito.mock(LocationResult.class);
+        Location location = Mockito.mock(Location.class);
+        Mockito.doReturn(location).when(locationResult).getLastLocation();
+        Mockito.doAnswer((Answer<Task<Void>>) invocation -> {
+            LocationCallback callback = invocation.getArgument(0);
+            callback.onLocationResult(locationResult);
+            return null;
+        }).when(fusedLocationProviderClient).removeLocationUpdates(any(LocationCallback.class));
         gpsLocationRepositoryImplementation.stopLocationUpdates();
 
         // When
-        Boolean result = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getIsLocationPermissionAllowedLiveData());
+        Location result = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getCurrentLocationLiveData());
+        Boolean isPermissionGranted = LiveDataTestUtil.getValueForTesting(gpsLocationRepositoryImplementation.getIsLocationPermissionAllowedLiveData());
 
         // Then
-        assertFalse(result);
+        assertEquals(location, result);
+        assertFalse(isPermissionGranted);
     }
 }
