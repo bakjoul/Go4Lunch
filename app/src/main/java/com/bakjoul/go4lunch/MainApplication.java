07fc0d0fd0ff7b6e2669828c31df00fc38e01e14
bakjoul <bakjoul@gmail.com>
2023-02-14 18:21:35 +0100

    Notifications working and showing at wanted time, need to customize them

--- app/src/main/java/com/bakjoul/go4lunch/MainApplication.java
+++ app/src/main/java/com/bakjoul/go4lunch/MainApplication.java
@@ -5,6 +5,15 @@ import android.app.Application;
 import androidx.annotation.NonNull;
 import androidx.hilt.work.HiltWorkerFactory;
 import androidx.work.Configuration;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import com.bakjoul.go4lunch.worker.NotificationWorker;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 
@@ -15,6 +24,34 @@ public class MainApplication extends Application implements Configuration.Provid
 
     @Inject
     HiltWorkerFactory workerFactory;
+
+    @Inject
+    Clock clock;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(
+            NotificationWorker.class,
+            1,
+            TimeUnit.DAYS
+        )
+            //.setInitialDelay(getDelayFromLunchTime(), TimeUnit.MILLISECONDS)
+            .build();
+
+        WorkManager.getInstance(this).enqueue(workRequest);
+    }
+
+    private long getDelayFromLunchTime() {
+        Duration delay = Duration.between(LocalTime.now(clock), LocalTime.of(17, 4));
+
+        if (delay.isNegative()) {
+            delay = delay.plusDays(1);
+        }
+
+        return delay.toMillis();
+    }
 
     @NonNull
     @Override
--- app/src/main/java/com/bakjoul/go4lunch/worker/NotificationWorker.java
+++ app/src/main/java/com/bakjoul/go4lunch/worker/NotificationWorker.java
@@ -1,7 +1,12 @@
 package com.bakjoul.go4lunch.worker;
 
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -11,6 +16,7 @@ import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
 import com.bakjoul.go4lunch.R;
+import com.bakjoul.go4lunch.ui.details.DetailsActivity;
 
 import dagger.assisted.Assisted;
 import dagger.assisted.AssistedInject;
@@ -19,6 +25,7 @@ import dagger.assisted.AssistedInject;
 public class NotificationWorker extends Worker {
 
     private static final String CHANNEL_ID = "CHANNEL_ID";
+    private static final int NOTIFICATION_ID = 1;
 
     @AssistedInject
     public NotificationWorker(
@@ -32,14 +39,32 @@ public class NotificationWorker extends Worker {
     @NonNull
     @Override
     public Result doWork() {
+        Intent intent = new Intent(getApplicationContext(), DetailsActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        PendingIntent pendingIntent = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        }
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
             .setSmallIcon(R.drawable.ic_hot_bowl)
             .setContentTitle("Title")
             .setContentText("Text")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
-        notificationManager.notify(1, builder.build());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(
+                new NotificationChannel(
+                    CHANNEL_ID,
+                    getApplicationContext().getString(R.string.app_name),
+                    NotificationManager.IMPORTANCE_DEFAULT
+                ));
+        }
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
 
         return Result.success();
     }
