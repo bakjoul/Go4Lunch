b09f09cdbba6415dc25a494870e5fa468d41a24d
bakjoul <bakjoul@gmail.com>
2023-03-14 15:08:51 +0100

    Refactored ChatItemViewType

---
+++ app/src/main/java/com/bakjoul/go4lunch/data/chat/ChatItemViewType.java
@@ -0,0 +1,7 @@
+package com.bakjoul.go4lunch.data.chat;
+
+public enum ChatItemViewType {
+    DATE_HEADER,
+    RECEIVED_MESSAGE,
+    SENT_MESSAGE
+}
--- app/src/main/java/com/bakjoul/go4lunch/data/chat/ChatMessageViewType.java
+++
@@ -1,7 +0,0 @@
-package com.bakjoul.go4lunch.data.chat;
-
-public enum ChatMessageViewType {
-    DATE_HEADER_VIEW_TYPE,
-    RECEIVED_MESSAGE_VIEW_TYPE,
-    SENT_MESSAGE_VIEW_TYPE
-}
