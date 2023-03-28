e7a0e5292f59006d5238db0552a7db84157280f0
bakjoul <bakjoul@gmail.com>
2023-03-28 13:44:16 +0200

    Removed unused import

--- app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsFragment.java
+++ app/src/main/java/com/bakjoul/go4lunch/ui/restaurants/RestaurantsFragment.java
@@ -1,7 +1,6 @@
 package com.bakjoul.go4lunch.ui.restaurants;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
